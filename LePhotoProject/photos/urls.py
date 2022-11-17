8cde623b014367a9032f7af643af0ef9b43549ae
steven le <fastkingz303@gmail.com>
2022-11-16 19:10:55 -0500

    fiinal product

--- LePhotoProject/photos/urls.py
+++ LePhotoProject/photos/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    #path('', views.home, name="home"),
+    path('', views.loginUser, name="login"),
     path('contact/', views.contact, name="contact"),
-    path('login/', views.loginUser, name="login"),
     path('logout/', views.logoutUser, name="logout"),
     path('register/', views.registerUser, name="register"),
     path('gallery/', views.gallery, name='gallery'),
